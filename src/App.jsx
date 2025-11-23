7ddfba6eb1da76aa391d860bdefdab2aa77e17d5
Sweety <rsweet375@gmail.com>
2025-11-23 23:58:28 +0530

    install react-router-dom package and add router path for the home page and add router provider, outlet for the same

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header/Header'
 
@@ -5,7 +6,7 @@ function App() {
 
   return (
     <div>
-      <Header />
+      <Outlet />
     </div>
   )
 }
