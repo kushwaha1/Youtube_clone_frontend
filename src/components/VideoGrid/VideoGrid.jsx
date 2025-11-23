ebd8baf202221e9f7cbaefc2f13710650813df42
Sweety <rsweet375@gmail.com>
2025-11-23 23:54:07 +0530

    created video grid component and add video card

---
+++ src/components/VideoGrid/VideoGrid.jsx
@@ -0,0 +1,16 @@
+import VideoCard from "../VideoCard/VideoCard";
+import { React } from "react";
+
+function VideoGrid({ videos }) {
+  return (
+    <div className="p-3 sm:p-4 lg:p-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 sm:gap-4">
+        {videos.map((video) => (
+          <VideoCard key={video.id} video={video} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default VideoGrid
